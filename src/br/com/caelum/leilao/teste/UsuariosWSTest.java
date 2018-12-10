79d867d6b92303bce6bed35dfef8c79b716d0101
Robson Marinho <robson_luz15@hotmail.com>
2018-12-10 01:56:15 -0300

    Consumindo e testando um webservice

---
+++ src/br/com/caelum/leilao/teste/UsuariosWSTest.java
@@ -0,0 +1,30 @@
+package br.com.caelum.leilao.teste;
+
+import static com.jayway.restassured.RestAssured.get;
+import static com.jayway.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import br.com.caelum.leilao.modelo.Usuario;
+import com.jayway.restassured.path.xml.XmlPath;
+
+public class UsuariosWSTest {
+
+	@Test
+	public void deveRetornarListaDeUsuarios() {
+		XmlPath path = 
+				given().
+				header("Accept", "application/xml")
+				.get("/usuarios").andReturn().xmlPath();
+		
+		Usuario usuario1 = path.getObject("list.usuario[0] ", Usuario.class);
+		Usuario usuario2 = path.getObject("list.usuario[1] ", Usuario.class);
+		
+		Usuario esperado1 = new Usuario(1L, "Mauricio Aniche", "mauricio.aniche@caelum.com.br");
+		Usuario esperado2 = new Usuario(2L, "Guilherme Silveira", "guilherme.silveira@caelum.com.br");
+		
+		assertEquals(esperado1, usuario1);
+		assertEquals(esperado2, usuario2);
+		
+	}
+}
